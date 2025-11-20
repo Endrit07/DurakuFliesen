1b5456f2194dd5663ae1e6b85d62a196956382f2
Endrit Duraku <156317428+Endrit07@users.noreply.github.com>
2025-11-20 19:58:22 +0100

    Add files via upload

---
+++ wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1696883746;s:7:"authKey";s:64:"39#q?W7{Z|(T;FP7$x[r^e[a/[Sd!k.`:(XTl_eSw?FMc,]&h~YwK~PX#}E8uA)U";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:3752;}
